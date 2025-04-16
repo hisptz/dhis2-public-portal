8e14ea921b92345e2014a489dfd0b16bbecbacf2
Gift Nnko <nnkogift@gmail.com>
2025-04-16 15:44:33 +0300

    chore: shared module Implemented appearance initial configuration schema

---
+++ packages/shared/src/schemas/appearance/index.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const appColorConfig = z.object({
+	primary: z.string(),
+	background: z.string(),
+	chartColors: z
+		.array(z.string())
+		.min(8, { message: "Must have at least 8 colors" }),
+});
+
+export type AppColorConfig = z.infer<typeof appColorConfig>;
+
+export const appAppearanceConfig = z.object({
+	colors: appColorConfig,
+});
