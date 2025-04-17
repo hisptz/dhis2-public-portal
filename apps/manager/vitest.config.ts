428ea46521186d57b14f17ee9d928024e1a33759
Gift Nnko <nnkogift@gmail.com>
2025-04-17 11:52:59 +0300

    chore(vitest): update configs for test setup and add specific manager configuration
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/manager/vitest.config.ts
+++ apps/manager/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		workspace: ["apps/*", "packages/*"],
 		include: ["**/*.test.ts?(x)"],
+		exclude: [".d2/*"],
 	},
 });
