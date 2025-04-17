353d0525cf7cc4b5a2e0d3e894c14ca55e5147b5
Gift Nnko <nnkogift@gmail.com>
2025-04-17 11:12:32 +0300

    fix(env): use safeParse to handle invalid environment variables gracefully
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/utils/env.ts
+++ apps/portal/src/utils/env.ts
@@ -5,4 +5,7 @@ const envSchema = z.object({
 	DHIS2_BASE_PAT_TOKEN: z.string(),
 });
 
-export const env = envSchema.safeParse(process.env).data!;
+export const env = envSchema.safeParse(process.env).data ?? {
+	DHIS2_BASE_URL: "",
+	DHIS2_BASE_PAT_TOKEN: "",
+};
