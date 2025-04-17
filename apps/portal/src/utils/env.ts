32ba956b24cfb3fcdc10902479d63d35d2998abf
Gift Nnko <nnkogift@gmail.com>
2025-04-17 11:15:43 +0300

    chore:  to be deleted
    Attempts to fix issues during CI
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/utils/env.ts
+++ apps/portal/src/utils/env.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
 	DHIS2_BASE_PAT_TOKEN: z.string(),
 });
 
+console.log(process.env);
 export const env = envSchema.safeParse(process.env).data ?? {
 	DHIS2_BASE_URL: "",
 	DHIS2_BASE_PAT_TOKEN: "",
