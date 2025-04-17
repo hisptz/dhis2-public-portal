3a05763bb0e14fa9b0a42e21b69242ad5e390e2f
Gift Nnko <nnkogift@gmail.com>
2025-04-17 11:21:36 +0300

    chore:  to be deleted
    Attempts to fix issues during CI
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/utils/env.ts
+++ apps/portal/src/utils/env.ts
@@ -1,4 +1,8 @@
 import { z } from "zod";
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
 
 const envSchema = z.object({
 	DHIS2_BASE_URL: z.string(),
