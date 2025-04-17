5db9e8243e3a0ca47a7e9a7059df8c45a41ee186
Gift Nnko <nnkogift@gmail.com>
2025-04-17 14:57:13 +0300

    feat(env): add support for optional `CONTEXT_PATH` environment variable
    - Updated `env.ts` to include `CONTEXT_PATH` as an optional environment variable.
    - Configured `turbo.json` to treat `CONTEXT_PATH` as a global environment variable.
    - Integrated `CONTEXT_PATH` as `basePath` in the Next.js configuration.
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/utils/env.ts
+++ apps/portal/src/utils/env.ts
@@ -7,9 +7,11 @@ loadEnvConfig(projectDir);
 const envSchema = z.object({
 	DHIS2_BASE_URL: z.string(),
 	DHIS2_BASE_PAT_TOKEN: z.string(),
+	CONTEXT_PATH: z.string().optional(),
 });
 
 export const env = envSchema.safeParse(process.env).data ?? {
 	DHIS2_BASE_URL: "http://localhost:8080",
 	DHIS2_BASE_PAT_TOKEN: "d2_pat-placeholder",
+	CONTEXT_PATH: ",
 };
