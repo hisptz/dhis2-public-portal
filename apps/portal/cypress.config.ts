2cb788710782c8d6497f0165f45622ae6f5674e9
Gift Nnko <nnkogift@gmail.com>
2025-04-18 18:43:16 +0300

    tests: Added tests for menu feature
    - The tests are disabled until we get the mock server working check a `pps/portal/cypress/README.md` for more info
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/cypress.config.ts
+++ apps/portal/cypress.config.ts
@@ -3,7 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			// on("before:spec", () => {
+			// 	return SSRLocalhostMocker.init(8080); // it'll initialize your servers. You can pass any ports you want on params, like: (3000, 3001, 3002, ...)
+			// });
+			// on("after:spec", () => {
+			// 	return SSRLocalhostMocker.close(); // it'll close server when necessary
+			// });
+			// on("task", {
+			// 	mockBackendRequest: SSRLocalhostMocker.getMockBackendRequest(), // it'll create a helper to mock requests
+			// 	clearAllbackendMockRequests:
+			// 		SSRLocalhostMocker.getClearAllMocks(), // it'll create a helper to clear mock requests
+			// });
+			return config;
 		},
 		baseUrl: "http://localhost:3000",
 		specPattern: "cypress/integration/**/*.cy.ts",
--- apps/portal/cypress/integration/home.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("home", () => {
-	it("should display welcome header", () => {
-		cy.visit("/");
-		cy.get("h1")
-			.contains("Welcome to DHIS2 Public Portal!")
-			.should("be.visible");
-	});
-});
