a4b976078efc1c92f02ad920225148303d65679b
Gift Nnko <nnkogift@gmail.com>
2025-04-18 14:15:34 +0300

    refactor(ui): replace <div> with Mantine <Box> for consistent styling in loading.tsx
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/app/(modules)/[...module]/loading.tsx
+++ apps/portal/src/app/(modules)/[...module]/loading.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { Loader } from "@mantine/core";
+import { Box, Loader } from "@mantine/core";
 
 export default function ModuleLoading() {
 	return (
-		<div className="w-full h-full flex items-center justify-center">
+		<Box
+			style={{
+				height: "calc(100dvh - 400px)",
+			}}
+			className="w-full flex items-center justify-center"
+		>
 			<Loader />
-		</div>
+		</Box>
 	);
 }
