0c4198fefadd451de277e9ee03b39a376aa3e943
Gift Nnko <nnkogift@gmail.com>
2025-04-18 20:41:46 +0300

    fix: Fixed initial route issue
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/app/route.ts
+++ apps/portal/src/app/route.ts
@@ -3,6 +3,7 @@ import { AppMenuConfig } from "@packages/shared/schemas";
 import { DatastoreNamespaces } from "@packages/shared/constants";
 import { sortBy } from "lodash";
 import { redirect } from "next/navigation";
+import { env } from "@/utils/env";
 
 export async function GET() {
 	const menuConfig = await getAppConfigWithNamespace<AppMenuConfig>({
@@ -13,8 +14,8 @@ export async function GET() {
 	const firstMenu = sortBy(menuConfig?.items, "sortOrder")[0];
 
 	if (firstMenu.type === "module") {
-		return redirect(firstMenu.path);
+		return redirect(`${env.CONTEXT_PATH}/${firstMenu.path}`);
 	}
-	const url = `${firstMenu.path}/${firstMenu.items[0].path}`;
+	const url = `${env.CONTEXT_PATH}/${firstMenu.path}/${firstMenu.items[0].path}`;
 	return redirect(url);
 }
