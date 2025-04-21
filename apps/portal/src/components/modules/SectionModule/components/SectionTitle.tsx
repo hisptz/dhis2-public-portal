dd6b8e66d799cbaf272b7712ce2873a7d1517f31
Gift Nnko <nnkogift@gmail.com>
2025-04-21 16:27:57 +0300

    refactor: remove unused theme color prop from SectionTitle component
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/components/modules/SectionModule/components/SectionTitle.tsx
+++ apps/portal/src/components/modules/SectionModule/components/SectionTitle.tsx
@@ -5,9 +5,5 @@ import { Title, useMantineTheme } from "@mantine/core";
 
 export function SectionTitle({ section }: { section: Section }) {
 	const theme = useMantineTheme();
-	return (
-		<Title order={3} c={theme.primaryColor}>
-			{section.title}
-		</Title>
-	);
+	return <Title order={3}>{section.title}</Title>;
 }
