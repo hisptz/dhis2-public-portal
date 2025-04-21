25898eba604fda81d25b388d36ab136a34947b55
Gift Nnko <nnkogift@gmail.com>
2025-04-21 16:34:04 +0300

    feat(details-page): add "Back" button with navigation to parent page
    Added a "Back" button using `Button` and `IconArrowLeft` to navigate to the parent page. Refactored layout with `Stack` for better alignment.
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/components/modules/StaticModule/components/DetailsPage.tsx
+++ apps/portal/src/components/modules/StaticModule/components/DetailsPage.tsx
@@ -1,10 +1,12 @@
-import { Card, Container, Title } from "@mantine/core";
+import { Button, Card, Container, Stack, Title } from "@mantine/core";
 import { getAppModule } from "@/utils/module";
 import { ModuleType, StaticItemConfig } from "@packages/shared/schemas";
 import { getAppConfigWithNamespace } from "@/utils/config";
 import { DatastoreNamespaces } from "@packages/shared/constants";
 import { BaseCardError } from "@/components/CardError";
 import { RichContent } from "@/components/RichContent";
+import { IconArrowLeft } from "@tabler/icons-react";
+import Link from "next/link";
 
 export async function DetailsPage({
 	id,
@@ -36,10 +38,20 @@ export async function DetailsPage({
 
 	return (
 		<Container fluid px={0}>
-			<Title order={2}>{item.title}</Title>
-			<Card>
-				<RichContent content={item.content} />
-			</Card>
+			<Stack align="flex-start">
+				<Button
+					variant="subtle"
+					href={"../"}
+					component={Link}
+					leftSection={<IconArrowLeft size={14} />}
+				>
+					Back
+				</Button>
+				<Title order={2}>{item.title}</Title>
+				<Card>
+					<RichContent content={item.content} />
+				</Card>
+			</Stack>
 		</Container>
 	);
 }
