5924a025abb4a65aeb2d6a783def6dc21093e438
Gift Nnko <nnkogift@gmail.com>
2025-04-22 16:16:35 +0300

    feat: enhance UI for menu and modules routes with `ModuleContainer` and add loading state
    - Updated `/menu/` and `/modules/` routes to use `ModuleContainer` for consistent layout
    - Introduced a `Suspense` fallback with `CircularLoader` for improved loading experience in the root component
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/manager/src/modules/__root.tsx
+++ apps/manager/src/modules/__root.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { Suspense } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { SideMenu } from "../shared/components/SideMenu/SideMenu";
+import { CircularLoader } from "@dhis2/ui";
 
 export const Route = createRootRoute({
 	component: RootComponent,
@@ -12,7 +14,15 @@ function RootComponent() {
 			<div className="h-full w-full flex">
 				<SideMenu />
 				<main className="flex-1 h-full p-[16px]">
-					<Outlet />
+					<Suspense
+						fallback={
+							<div className="h-full w-full flex justify-center items-center">
+								<CircularLoader />
+							</div>
+						}
+					>
+						<Outlet />
+					</Suspense>
 				</main>
 			</div>
 		</React.Fragment>
--- apps/manager/src/modules/menu/index.lazy.tsx
+++ apps/manager/src/modules/menu/index.lazy.tsx
@@ -1,10 +1,15 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import React from "react";
+import { ModuleContainer } from "../../shared/components/ModuleContainer";
 
 export const Route = createLazyFileRoute("/menu/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/menu/"!</div>;
+	return (
+		<ModuleContainer title="App Menu">
+			<div className="w-full h-full">A table of menu items</div>
+		</ModuleContainer>
+	);
 }
