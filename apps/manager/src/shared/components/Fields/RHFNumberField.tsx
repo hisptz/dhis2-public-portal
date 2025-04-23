518c2c97b4ef710091fdd5d93d59e8a8b8baa403
Allen7702 <allen22052000@gmail.com>
2025-04-23 12:58:11 +0300

    feat(fields): add various RHF components for form handling

---
+++ apps/manager/src/shared/components/Fields/RHFNumberField.tsx
@@ -0,0 +1,36 @@
+import { InputField, InputFieldProps } from "@dhis2/ui";
+import { RHFFieldProps } from "@hisptz/dhis2-ui/dist/types/forms/interfaces";
+import React from "react";
+import { useController } from "react-hook-form";
+
+export type RHFNumberFieldProps = InputFieldProps & RHFFieldProps & {};
+
+export function RHFNumberField({
+	name,
+	validations,
+	...props
+}: RHFNumberFieldProps) {
+	const { field, fieldState } = useController({
+		name,
+		rules: validations,
+	});
+
+	return (
+		<InputField
+			{...field}
+			ref={field.ref}
+			error={!!fieldState.error}
+			validationText={fieldState.error?.message}
+			value={field.value?.toString()}
+			onChange={({ value }) => {
+				if (value) {
+					field.onChange(+value);
+				} else {
+					field.onChange(null);
+				}
+			}}
+			type="number"
+			{...props}
+		/>
+	);
+}
