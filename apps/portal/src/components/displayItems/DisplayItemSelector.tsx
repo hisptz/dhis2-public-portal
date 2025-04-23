9c4a7594190f79edc2ed2114b92732226fa1c7e9
Gift Nnko <nnkogift@gmail.com>
2025-04-23 19:12:48 +0300

    feat(displayItems): introduce `RichTextVisualizer` component
    - Replaced `RichContent` usage with the new `RichTextVisualizer`.
    - Added the `RichTextVisualizer` component for displaying rich text items, ensuring consistency.
    - Updated `richText` schema for better type management and separation of concerns.
    - Applied minor adjustments to map visualization to prevent duplicate map views by using `uniqBy`.
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/components/displayItems/DisplayItemSelector.tsx
+++ apps/portal/src/components/displayItems/DisplayItemSelector.tsx
@@ -1,7 +1,7 @@
 import { DisplayItem, DisplayItemType } from "@packages/shared/schemas";
 import { MainVisualization } from "@/components/displayItems/visualizations/MainVisualization";
 import { SingleValueVisualizer } from "@/components/displayItems/SingleValueVisualizer/SingleValueVisualizer";
-import { RichContent } from "@/components/RichContent";
+import { RichTextVisualizer } from "@/components/displayItems/RichTextVisualizer";
 
 export function DisplayItemSelector({ item }: { item: DisplayItem }) {
 	switch (item.type) {
@@ -15,13 +15,7 @@ export function DisplayItemSelector({ item }: { item: DisplayItem }) {
 		case DisplayItemType.SINGLE_VALUE:
 			return <SingleValueVisualizer config={item.item} />;
 		case DisplayItemType.RICH_TEXT:
-			return (
-				<div className="flex flex-col gap-4 w-full h-full">
-					<div className="flex-1">
-						<RichContent content={item.item.content} />
-					</div>
-				</div>
-			);
+			return <RichTextVisualizer item={item.item} />;
 		default:
 			return <div>Display Type not supported yet</div>;
 	}
