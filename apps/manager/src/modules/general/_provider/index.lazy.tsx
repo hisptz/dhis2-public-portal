c97754c3e114bcfaab8393a6913b4b2ad3f34a22
Allen7702 <allen22052000@gmail.com>
2025-04-24 08:52:40 +0300

    refactor(modules): remove unused metadata hooks from general and menu routes

--- apps/manager/src/modules/general/_provider/index.lazy.tsx
+++ apps/manager/src/modules/general/_provider/index.lazy.tsx
@@ -2,15 +2,13 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 import i18n from "@dhis2/d2-i18n";
 import React from "react";
 import { ModuleContainer } from "../../../shared/components/ModuleContainer";
-import { useMetadata } from "../../../shared/components/GeneralPage/providers/GeneralProvider";
 
 export const Route = createLazyFileRoute("/general/_provider/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	const data = useMetadata();
-	console.log("data", data);
+
 	return (
 		<ModuleContainer title={i18n.t("General configuration")}>
 			<div>
--- apps/manager/src/modules/menu/_provider/index.lazy.tsx
+++ apps/manager/src/modules/menu/_provider/index.lazy.tsx
@@ -1,15 +1,12 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import React from "react";
 import { ModuleContainer } from "../../../shared/components/ModuleContainer";
-import { useMenu } from "../../../shared/components/MenuPage/providers/MenuProvider";
 
 export const Route = createLazyFileRoute("/menu/_provider/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	const data = useMenu();
-		console.log("data", data);
 	return (
 		<ModuleContainer title="App Menu">
 			<div className="w-full h-full">A table of menu items</div>
