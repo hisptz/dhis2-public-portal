42c8e215aca3c11a211f26d41e7b2aabce7cc032
Eric Chingalo <echingalo@gmail.com>
2025-04-24 19:48:45 +0300

    feat(modules/appearance): added query for the appearance configurations

--- apps/manager/src/modules/appearance/index.lazy.tsx
+++ apps/manager/src/modules/appearance/index.lazy.tsx
@@ -2,44 +2,63 @@ import { createLazyFileRoute } from "@tanstack/react-router";
 import React from "react";
 import { ModuleContainer } from "../../shared/components/ModuleContainer";
 import i18n from "@dhis2/d2-i18n";
+import { useDataQuery } from "@dhis2/app-runtime";
+import { CircularLoader, NoticeBox } from "@dhis2/ui";
+import { AppAppearanceConfig } from "@packages/shared/schemas";
+import { AppearanceConfig } from "../../shared/components/appearance/AppearanceConfig/AppearanceConfig";
+import { MissingAppearanceConfig } from "../../shared/components/appearance/MissingAppearanceConfig";
+
+const query = {
+	appearanceConfig: {
+		resource: "dataStore",
+		id: "hisptz-public-portal/appearance",
+	},
+};
+
+type QueryResult = {
+	appearanceConfig: AppAppearanceConfig;
+};
 
 export const Route = createLazyFileRoute("/appearance/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const { loading, data, error } = useDataQuery<QueryResult>(query);
+
+	if (error) {
+		return (
+			<NoticeBox
+				title={i18n.t("Could not get Appearance configurations")}
+			>
+				{
+					<div className="flex flex-col gap-2">
+						<p>
+							{i18n.t("Error")}: {error.message}
+						</p>
+					</div>
+				}
+			</NoticeBox>
+		);
+	}
+
+	const { appearanceConfig } = data ?? {};
+
 	return (
 		<ModuleContainer title="Appearance">
-			<section>
-				<h3 className="text-lg font-bold">
-					{i18n.t("Application colors")}
-				</h3>
-				<div>
-					<ul>
-						<li>
-							Primary color - Selector of available colors -
-							Mantine colors
-						</li>
-
-						<li>
-							Custom colors - Add one color and generate the
-							shades
-						</li>
-					</ul>
+			{loading ? (
+				<div className="flex justify-center items-center h-50">
+					<CircularLoader />
 				</div>
-				<h4>Chart colors</h4>
-				Selection of colors for chart visualizations
-			</section>
-			<section>
-				<h3 className="text-lg font-bold">
-					{i18n.t("Header configuration")}
-				</h3>
-			</section>
-			<section>
-				<h3 className="text-lg font-bold">
-					{i18n.t("Footer configuration")}
-				</h3>
-			</section>
+			) : appearanceConfig ? (
+				<AppearanceConfig appearanceConfig={appearanceConfig} />
+			) : (
+				<MissingAppearanceConfig
+					onAddConfigurations={() =>
+						console.log("Add configurations")
+					}
+				/>
+			)}
 		</ModuleContainer>
 	);
 }
