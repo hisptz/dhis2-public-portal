fe7e815a80bba9a8a2455bcd4515a596c9e88885
Gift Nnko <nnkogift@gmail.com>
2025-04-28 17:21:15 +0300

    chore:  cleanup
    Removed unused pages
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/app/(modules)/help-and-feedback/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@mantine/core";
-
-export default function Loading() {
-	return (
-		<div className="w-full h-full flex items-center justify-center min-h-[400px]">
-			<Loader color="blue" />
-		</div>
-	);
-}
