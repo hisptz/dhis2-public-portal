cb6c71311e5aa514bdc4e66901104985e6ebfe9e
Allen7702 <allen22052000@gmail.com>
2025-04-30 11:15:15 +0300

    feat(index.ts): include feedbackDisplayItemSchema in displayItemSchema

--- packages/shared/src/schemas/displayItems/index.ts
+++ packages/shared/src/schemas/displayItems/index.ts
@@ -2,15 +2,18 @@ import { visualizationDisplayItemSchema } from "./visualization";
 import { z } from "zod";
 import { richTextDisplayItemSchema } from "./richText";
 import { singleValueDisplayItemSchema } from "./singleValue";
+import { feedbackDisplayItemSchema } from "./feedback";
 
 export * from "./base";
 export * from "./richText";
 export * from "./singleValue";
+export * from "./feedback";
 export * from "./visualization";
 
 export const displayItemSchema = z.discriminatedUnion("type", [
 	visualizationDisplayItemSchema,
 	richTextDisplayItemSchema,
 	singleValueDisplayItemSchema,
+	feedbackDisplayItemSchema
 ]);
 export type DisplayItem = z.infer<typeof displayItemSchema>;
