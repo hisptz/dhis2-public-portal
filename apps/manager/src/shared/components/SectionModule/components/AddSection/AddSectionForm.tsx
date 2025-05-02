db090191802b6a2c909e8ddcaaea44921f0b5943
tibendadavis <davistibz@gmail.com>
2025-05-02 12:49:09 +0300

    feat(AddSectionForm): implement success and error alerts on section creation

--- apps/manager/src/shared/components/SectionModule/components/AddSection/AddSectionForm.tsx
+++ apps/manager/src/shared/components/SectionModule/components/AddSection/AddSectionForm.tsx
@@ -19,6 +19,8 @@ import {
 } from "@packages/shared/schemas";
 import { startCase } from "lodash";
 import { SectionIDField } from "./SectionIDField";
+import { FetchError, useAlert } from "@dhis2/app-runtime";
+import { useUpdateModule } from "../../hooks/save";
 
 export function AddSectionForm({
 	sortOrder,
@@ -31,6 +33,11 @@ export function AddSectionForm({
 	onClose: () => void;
 	onAdd: (section: BaseSectionConfig) => void;
 }) {
+	const { updateModule } = useUpdateModule();
+	const { show } = useAlert(
+		({ message }) => message,
+		({ type }) => ({ ...type, duration: 3000 }),
+	);
 	const form = useForm<BaseSectionConfig>({
 		resolver: zodResolver(baseSectionSchema),
 		defaultValues: {
@@ -38,9 +45,22 @@ export function AddSectionForm({
 		},
 	});
 
-	const onSubmit = (data: BaseSectionConfig) => {
-		onAdd(data);
-		onClose();
+	const onSubmit = async (data: BaseSectionConfig) => {
+		try {
+			onAdd(data);
+			show({
+				message: i18n.t("Section created successfully"),
+				type: { success: true },
+			});
+			onClose();
+		} catch (e) {
+			if (e instanceof FetchError || e instanceof Error) {
+				show({
+					message: `${i18n.t("Could not create new item")}: ${e.message ?? e.toString()}`,
+					type: { critical: true },
+				});
+			}
+		}
 	};
 
 	const displayType = useWatch({ name: "sectionDisplay" });
