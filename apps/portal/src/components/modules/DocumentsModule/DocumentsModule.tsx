304bfdeb056ae6259370fd419165f4bc3cbbe982
tibendadavis <davistibz@gmail.com>
2025-05-06 14:11:52 +0300

    fix(DocumentsModule): prevent rendering group control for empty groups

--- apps/portal/src/components/modules/DocumentsModule/DocumentsModule.tsx
+++ apps/portal/src/components/modules/DocumentsModule/DocumentsModule.tsx
@@ -2,6 +2,7 @@ import { Stack, Title } from "@mantine/core";
 import { DocumentsItemsContainer } from "@/components/modules/DocumentsModule/components/DocumentsItemsContainer";
 import { DocumentsModuleConfig } from "@packages/shared/schemas";
 import { DocumentsGroupControl } from "@/components/modules/DocumentsModule/components/DocumentsGroupControl";
+import { isEmpty } from "lodash";
 
 export function DocumentsModule({
 	config,
@@ -13,7 +14,9 @@ export function DocumentsModule({
 	return (
 		<Stack className="w-full h-full">
 			<Title order={2}>{config.title}</Title>
-			{config.grouped && <DocumentsGroupControl config={config} />}
+			{config.grouped && !isEmpty(config.groups) && (
+				<DocumentsGroupControl config={config} />
+			)}
 			<DocumentsItemsContainer
 				config={config}
 				searchParams={searchParams}
