8f6c68653a63b90223d5ecbf21f7fa7f6c362a1c
Gift Nnko <nnkogift@gmail.com>
2025-05-07 14:53:47 +0300

    feat: add DHIS2 version compatibility to manager app config`
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/manager/d2.config.js
+++ apps/manager/d2.config.js
@@ -1,10 +1,13 @@
 const config = {
+	id: "",
 	type: "app",
 	title: "Portal Manager",
 	name: "portal-manager",
 	entryPoints: {
 		app: "./src/App.tsx",
 	},
+	minDHIS2Version: "2.40",
+	maxDHIS2Version: "2.42",
 };
 
 module.exports = config;
