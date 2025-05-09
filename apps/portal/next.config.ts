d2b4896ad3fd3db59248dde94023c43a6eaa824e
Gift Nnko <nnkogift@gmail.com>
2025-05-09 17:13:06 +0300

    fix(portal): next.config.ts fixes
    Fixed issues with undefined context path causing issues during build
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/next.config.ts
+++ apps/portal/next.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
-	basePath: process.env.CONTEXT_PATH,
+	basePath: process.env.CONTEXT_PATH || undefined,
 	images: {
 		remotePatterns: [
 			{
