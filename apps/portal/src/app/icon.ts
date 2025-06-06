cf3c425a2b7370625f32e450e2f9d72fe503e370
Gift Nnko <nnkogift@gmail.com>
2025-06-06 13:16:07 +0300

    chore: remove unused `favicons` dependency and related logic
    - Removed `favicons` dependency from `package.json`.
    - Deleted `faviconOptions` and associated generation logic in `icon.ts`.
    - Simplified icon response handling in `icon.ts` by returning the blob directly.
    - Updated `yarn.lock` to reflect dependency removal.
    
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/app/icon.ts
+++ apps/portal/src/app/icon.ts
@@ -2,19 +2,7 @@ import { getAppConfigWithNamespace } from "@/utils/config";
 import { AppMeta } from "@packages/shared/schemas";
 import { DatastoreNamespaces } from "@packages/shared/constants";
 import { dhis2HttpClient } from "@/utils/api/dhis2";
-import { FaviconOptions, favicons } from "favicons";
 import { NextResponse } from "next/server";
-
-const faviconOptions: FaviconOptions = {
-	icons: {
-		favicons: false,
-		appleIcon: true,
-		android: false,
-		windows: false,
-		appleStartup: false,
-		yandex: false,
-	},
-};
 
 export const size = {
 	width: 32,
@@ -28,24 +16,12 @@ export default async function iconGenerator() {
 		key: "metadata",
 	});
 	if (!config) {
-		return null;
+		return NextResponse.error();
 	}
 	if (!config.icon) {
-		return null;
+		return NextResponse.error();
 	}
 
 	const icon = await dhis2HttpClient.getRaw(`documents/${config.icon}/data`);
-	const iconBuffer = await (await icon.blob()).arrayBuffer();
-	const generatedIcons = await favicons(
-		Buffer.from(iconBuffer),
-		faviconOptions,
-	);
-
-	const generatedIcon = generatedIcons.images.find((image) =>
-		image.name.includes(".png"),
-	);
-
-	return new NextResponse(
-		new Blob([generatedIcon?.contents!], { type: "image/png" }),
-	);
+	return new NextResponse(await icon.blob());
 }
