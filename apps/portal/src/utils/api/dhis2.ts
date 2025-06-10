5ae4432aed3dcfe16b5fc25b8955acaef816c64e
Gift Nnko <nnkogift@gmail.com>
2025-06-10 11:14:20 +0300

    fix: implements sanitization to the base URL set by the user
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/portal/src/utils/api/dhis2.ts
+++ apps/portal/src/utils/api/dhis2.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "./http";
+import { D2HttpClient } from "./http";
 import { env } from "@/utils/env";
 
-export const dhis2HttpClient = new HttpClient(
-	`${env.DHIS2_BASE_URL}/api/`,
+export const dhis2HttpClient = new D2HttpClient(
+	env.DHIS2_BASE_URL,
 	env.DHIS2_BASE_PAT_TOKEN,
 );
