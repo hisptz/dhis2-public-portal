a3036271b6d1c9fb5a75b17ddbe2bcc53dfc149f
tibendadavis <davistibz@gmail.com>
2025-07-24 13:27:09 +0300

    feat(data-service-configuration)

--- packages/shared/src/schemas/data-service/config.ts
+++ packages/shared/src/schemas/data-service/config.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
+import i18n from "@dhis2/d2-i18n";
 
 export const dataSourceSchema = z.object({
 	routeId: z.string(),
 	name: z.string(),
-	version: z.string(),
 });
 
 export type DataServiceDataSource = z.infer<typeof dataSourceSchema>;
@@ -15,19 +15,22 @@ export const dataItemConfigSchema = z.object({
 
 export type DataServiceDataItemConfig = z.infer<typeof dataItemConfigSchema>;
 
-export const supportedDataSourcesType = z.enum([
-	"ATTRIBUTE_VALUES",
-	"DX_VALUES",
-]);
+export enum DataServiceSupportedDataSourcesType {
+	ATTRIBUTE_VALUES = "ATTRIBUTE_VALUES",
+	DX_VALUES = "DX_VALUES",
+}
 
-export type DataServiceSupportedDataSourcesType = z.infer<
-	typeof supportedDataSourcesType
->;
+export const supportedDataSourcesType = z.nativeEnum(
+	DataServiceSupportedDataSourcesType,
+);
 
 export const baseDataItemsSourceSchema = z.object({
 	id: z.string(),
+	name: z.string(),
 	type: supportedDataSourcesType,
-	dataItems: z.array(dataItemConfigSchema),
+	dataItems: z
+		.array(dataItemConfigSchema)
+		.min(1, i18n.t("At least one data item is required")),
 	periodTypeId: z.string(),
 	parentOrgUnitId: z.string(),
 	orgUnitLevel: z.number(),
@@ -52,10 +55,11 @@ export type DataServiceDxValuesDataItemsSource = z.infer<
 	typeof dxValuesDataItemsSourceSchema
 >;
 
-export const dataSourceItemsConfigSchema = z.discriminatedUnion("type", [
-	attributeValuesDataItemsSourceSchema,
-	dxValuesDataItemsSourceSchema,
-]);
+export const dataSourceItemsConfigSchema = z.discriminatedUnion(
+	"type",
+	[attributeValuesDataItemsSourceSchema, dxValuesDataItemsSourceSchema],
+	{ message: i18n.t("This value is required") },
+);
 
 export type DataServiceDataSourceItemsConfig = z.infer<
 	typeof dataSourceItemsConfigSchema
@@ -65,6 +69,7 @@ export const dataServiceConfigSchema = z.object({
 	id: z.string(),
 	source: dataSourceSchema,
 	itemsConfig: z.array(dataSourceItemsConfigSchema),
+	visualizations: z.array(z.object({ id: z.string() })),
 });
 
 export type DataServiceConfig = z.infer<typeof dataServiceConfigSchema>;
@@ -73,6 +78,7 @@ export const dataServiceRuntimeConfig = z.object({
 	periods: z.string().array(),
 	pageSize: z.number().optional(),
 	timeout: z.number().optional(),
+	paginateByData: z.boolean().optional(),
 	overrides: z
 		.object({
 			parentOrgUnitId: z.string().optional(),
@@ -87,3 +93,5 @@ export const dataDownloadBodySchema = z.object({
 	runtimeConfig: dataServiceRuntimeConfig,
 	dataItemsConfigIds: z.string().array(),
 });
+
+export type DataDownloadBody = z.infer<typeof dataDownloadBodySchema>;
