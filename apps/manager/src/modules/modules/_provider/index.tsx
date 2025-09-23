286bfadeb4d359d0ecc8b737b2d4dc6a85cb4729
Gift Nnko <nnkogift@gmail.com>
2025-09-23 14:04:12 +0300

    fix: fixed issues with filtering of modules causing infinite loop
    Signed-off-by: Gift Nnko <nnkogift@gmail.com>

--- apps/manager/src/modules/modules/_provider/index.tsx
+++ apps/manager/src/modules/modules/_provider/index.tsx
@@ -1,5 +1,9 @@
-import { createFileRoute, useNavigate, useSearch } from "@tanstack/react-router";
-import React, { useEffect, useState } from "react";
+import {
+	createFileRoute,
+	useNavigate,
+	useSearch,
+} from "@tanstack/react-router";
+import React, { useDeferredValue } from "react";
 import { ModuleContainer } from "../../../shared/components/ModuleContainer";
 import i18n from "@dhis2/d2-i18n";
 import { Header } from "../../../shared/components/ModulesPage/components/Header";
@@ -11,25 +15,14 @@ import { ModuleType } from "@packages/shared/schemas";
 export const Route = createFileRoute("/modules/_provider/")({
 	component: RouteComponent,
 	validateSearch: typeFilterSchema,
-
 });
 
 function RouteComponent() {
 	const search = useSearch({ from: "/modules/_provider/" });
-	const type = search.type as ModuleType | undefined;
+	const type = useDeferredValue(search.type);
 	const navigate = useNavigate({ from: Route.fullPath });
 
-	const [filterType, setFilterType] = useState<ModuleType | undefined>(type);
-
-    useEffect(() => {
-        if (type !== filterType) {
-            setFilterType(type);
-        }
-    }, [filterType, type]);
-
-
 	const handleTypeChange = (newType: ModuleType | undefined) => {
-        setFilterType(newType);
 		navigate({
 			search: (prev) => ({ ...prev, type: newType }),
 			replace: true,
@@ -38,15 +31,15 @@ function RouteComponent() {
 
 	return (
 		<ModuleContainer title={i18n.t("Modules")}>
-			<div className="w-full h-full flex flex-col py-4 px-8"> 
+			<div className="w-full h-full flex flex-col py-4 px-8">
 				<Header
 					actions={<AddModule />}
-                    selectedType={filterType} 
+					selectedType={search.type}
 					onTypeChange={handleTypeChange}
 				/>
-				<div className="flex-grow overflow-auto"> 
-                    <ModuleList filterType={filterType} />
-                </div>
+				<div className="flex-grow overflow-auto">
+					<ModuleList filterType={type} />
+				</div>
 			</div>
 		</ModuleContainer>
 	);
