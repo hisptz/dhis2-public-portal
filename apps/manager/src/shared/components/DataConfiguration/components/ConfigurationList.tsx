7b0b05e866cb8d677d4144b2fb3567be527eb973
Allen7702 <allen22052000@gmail.com>
2025-10-15 10:23:16 +0300

    refactor(ConfigurationList): replace action buttons with ActionsMenu component

--- apps/manager/src/shared/components/DataConfiguration/components/ConfigurationList.tsx
+++ apps/manager/src/shared/components/DataConfiguration/components/ConfigurationList.tsx
@@ -1,13 +1,10 @@
 import { SimpleTable, SimpleTableColumn } from "@hisptz/dhis2-ui";
 import { useDataSources } from "../providers/DataSourcesProvider";
 import React from "react";
-import { Button, ButtonStrip, IconSettings16, Tooltip } from "@dhis2/ui";
-import { useNavigate } from "@tanstack/react-router";
 import i18n from "@dhis2/d2-i18n";
-import { RunConfiguration } from "./RunConfiguration/RunConfiguration";
 import { RunConfigStatus } from "./RunConfiguration/components/RunConfigStatus/RunConfigStatus";
-import { RunConfigSummary } from "./RunConfiguration/components/RunConfigSummary/RunConfigSummary";
 import { AddDataSource } from "./AddDataSource";
+import { ActionsMenu } from "./ActionsMenu";
 
 const columns: SimpleTableColumn[] = [
 	{
@@ -26,34 +23,12 @@ const columns: SimpleTableColumn[] = [
 
 export function ConfigurationList() {
 	const configurations = useDataSources();
-	const navigate = useNavigate({
-		from: "/data-service-configuration/",
-	});
 
 	const rows = configurations.map((configuration) => ({
 		...configuration,
 		name: configuration.source.name,
 	    status: <RunConfigStatus configId={configuration.id} />,
-		actions: (
-			<ButtonStrip>
-				<Tooltip content={i18n.t("Edit configuration")}>
-					<Button
-						small
-						onClick={() => {
-							navigate({
-								to: "/data-service-configuration/$configId",
-								params: {
-									configId: configuration.id,
-								},
-							});
-						}}
-						icon={<IconSettings16 />}
-					/>
-				</Tooltip>
-				<RunConfiguration config={configuration} />
-				<RunConfigSummary config={configuration} />
-			</ButtonStrip>
-		),
+		actions: <ActionsMenu config={configuration} />,
 	}));
 
 	return (
