97b6d4b794f0ad4d2342d2f2a8b214e5db7c1d16
Allen7702 <allen22052000@gmail.com>
2025-10-20 16:25:08 +0300

    feat(dhis2): add metadata upload functionality and error handling feat(visualizations): implement visualization and map configuration retrieval feat(queue): introduce dynamic queue name generation

--- services/data-service/src/clients/dhis2.ts
+++ services/data-service/src/clients/dhis2.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { config } from "dotenv";
 import { env } from "@/env";
 import { DataServiceConfig } from "@packages/shared/schemas";
+import logger from "@/logging";
+import { uniqBy } from "lodash";
 
 config();
 
@@ -62,4 +64,114 @@ export function getDHIS2ClientByBasicAuth({
 			Accept: "application/json",
 		},
 	});
+}
+
+export async function uploadMetadataFile(filePath: string) {
+	const url =
+		"metadata?importStrategy=CREATE_AND_UPDATE&async=false&atomicMode=NONE";
+
+	if (!filePath.endsWith(".json")) {
+		logger.error(`${filePath}: Only JSON files are supported.`);
+		throw new Error("Only JSON files are supported for metadata upload.");
+	}
+
+	logger.info(`Starting upload: ${filePath}`);
+
+	try {
+		const payload = await Bun.file(filePath).json();
+
+		const response = await dhis2Client.post(url, payload, {
+			headers: { "Content-Type": "application/json" },
+			timeout: 30000,
+		});
+
+		logger.info(`Upload completed Successfully`);
+		logger.info(
+			`${JSON.stringify(
+				response.data.response.stats || response.data,
+				null,
+				2
+			)}`
+		);
+
+		return response.data;
+	} catch (err: any) {
+		const payload = await Bun.file(filePath).json();
+
+		await handleImportErrors(payload, err.response?.data);
+
+		logger.error(`Error uploading ${filePath}`);
+		logger.error(
+			`${JSON.stringify(
+				err.response?.data?.response.stats || err.message,
+				null,
+				2
+			)}`
+		);
+	}
+}
+
+export async function handleImportErrors(
+	originalPayload: any,
+	importResponse: any
+) {
+	const errorPath = "logs/errors.json";
+
+	let existingLog: any = {};
+	try {
+		const file = Bun.file(errorPath);
+		if (await file.exists()) {
+			existingLog = await file.json();
+		}
+	} catch (e: any) {
+		console.warn(
+			`Could not read existing error log, starting new: ${e.message}`
+		);
+	}
+
+	const typeReports: any[] = importResponse.response?.typeReports || [];
+	if (typeReports.length === 0) return;
+
+	const uidMap: any = {};
+	for (const [key, items] of Object.entries(originalPayload)) {
+		for (const obj of items as any[]) {
+			if (obj.id) {
+				uidMap[obj.id] = { key, obj };
+			}
+		}
+	}
+
+	for (const typeReport of typeReports) {
+		if (!typeReport.objectReports) continue;
+
+		for (const objReport of typeReport.objectReports) {
+			const failedUid = objReport.uid;
+			const match = uidMap[failedUid];
+
+			if (!match) {
+				console.warn(`UID ${failedUid} not found in payload.`);
+				continue;
+			}
+
+			const { key, obj } = match;
+
+			if (!existingLog[key]) {
+				existingLog[key] = [];
+			}
+
+			const errorMessages = (objReport.errorReports || []).map(
+				(e: any) => e.message
+			);
+
+			const failedObject = {
+				errorReports: errorMessages,
+				...obj,
+			};
+
+			existingLog[key].push(failedObject);
+			existingLog[key] = uniqBy(existingLog[key], "id");
+		}
+	}
+
+	await Bun.write(errorPath, JSON.stringify(existingLog, null, 2));
 }
---
+++ services/data-service/src/utils/visualizations.ts
@@ -0,0 +1,304 @@
+import { dhis2Client } from "@/clients/dhis2";
+import * as _ from "lodash";
+
+enum DisplayItemType {
+  RICH_TEXT = "RICH_TEXT",
+  VISUALIZATION = "VISUALIZATION",
+  HIGHLIGHTED_SINGLE_VALUE = "HIGHLIGHTED_SINGLE_VALUE",
+  FEEDBACK = "FEEDBACK",
+  SECTION = "SECTION",
+}
+
+export enum VisualizationDisplayItemType {
+  CHART = "CHART",
+  MAP = "MAP",
+  BANNER = "BANNER",
+}
+
+type Visualization = {
+  id: string;
+  type: string;
+};
+type Dashboard = {
+  visualizations: Visualization[];
+  groups: {
+    visualizations: Visualization[];
+    subGroups: {
+      visualizations: Visualization[];
+    }[];
+  }[];
+};
+
+type ModuleConfig = any;
+
+export async function getModuleConfigs() {
+  try {
+    const url = `dataStore/hisptz-public-portal-modules`;
+    const response = await dhis2Client.get<Array<String>>(url);
+    const moduleIds = response.data ?? [];
+    const moduleConfigs = [];
+    for (const moduleId of moduleIds) {
+      const moduleUrl = `dataStore/hisptz-public-portal-modules/${moduleId}`;
+      const moduleResponse = await dhis2Client.get<{
+        config: ModuleConfig;
+        type: DisplayItemType;
+      }>(moduleUrl);
+
+      if (!moduleResponse.data || !moduleResponse.data.config) {
+        console.warn(`Module config for ${moduleId} is empty or undefined`);
+        continue;
+      }
+      if (moduleResponse.data.type == DisplayItemType.VISUALIZATION) {
+        if (moduleResponse.data.config.grouped) {
+          for (const group of moduleResponse.data.config.groups) {
+            moduleConfigs.push(...group.items.map((item: any) => item.item));
+          }
+        } else {
+          moduleConfigs.push(
+            ...moduleResponse.data.config.items.map((item: any) => item.item)
+          );
+        }
+      }
+      if (moduleResponse.data.type == DisplayItemType.SECTION) {
+        for (const section of moduleResponse.data.config.sections) {
+          if (
+            section.type == "SINGLE_ITEM" &&
+            (section.item.type == DisplayItemType.VISUALIZATION ||
+              section.item.type == DisplayItemType.HIGHLIGHTED_SINGLE_VALUE)
+          ) {
+            moduleConfigs.push(section.item.item);
+          } else {
+            if (section.type != "SINGLE_ITEM") {
+              moduleConfigs.push(
+                ...section.items.map((item: any) => item.item)
+              );
+            }
+          }
+        }
+      }
+    }
+    return moduleConfigs.flat().filter((config) => config != undefined) ?? [];
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+ 
+
+export function getVisualizations(
+  moduleConfigs: { id: string; type: VisualizationDisplayItemType }[]
+) {
+  const visualizations = [];
+  visualizations.push(
+    ...(moduleConfigs?.filter(
+      (module) => module.type != VisualizationDisplayItemType.MAP
+    ) ?? [])
+  );
+  return visualizations;
+}
+
+export function getMaps(
+  moduleConfigs: { id: string; type: VisualizationDisplayItemType }[]
+) {
+  const maps = [];
+  maps.push(
+    ...(moduleConfigs?.filter(
+      (module) => module.type == VisualizationDisplayItemType.MAP
+    ) ?? [])
+  );
+  return maps;
+}
+
+export async function getVisualizationConfigs(visualizations: Visualization[]) {
+  if (visualizations.length < 30) {
+    const response = await dhis2Client.get<{ visualizations: any[] }>(
+      `visualizations`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${visualizations.map((vis) => vis.id).join(",")}]`,
+          paging: false,
+        },
+      }
+    );
+    return response.data?.visualizations;
+  }
+  const visualizationConfigs = [];
+  const chunked = _.chunk(visualizations, 30);
+  for (const chunk of chunked) {
+    const ids = chunk.map((vis) => vis.id).join(",");
+    const response = await dhis2Client.get<{ visualizations: any[] }>(
+      `visualizations`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${ids}]`,
+          paging: false,
+        },
+      }
+    );
+    visualizationConfigs.push(...response.data.visualizations);
+  }
+  return visualizationConfigs;
+}
+
+export async function getMapsConfig(maps: Visualization[]) {
+  if (maps.length < 30) {
+    const response = await dhis2Client.get<{ maps: any[] }>(`maps`, {
+      params: {
+        fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+        filter: `id:in:[${maps.map((vis) => vis.id).join(",")}]`,
+        paging: false,
+      },
+    });
+    return response.data.maps;
+  }
+  const mapConfigs = [];
+  const chunked = _.chunk(maps, 30);
+  for (const chunk of chunked) {
+    const response = await dhis2Client.get<{ maps: any[] }>(`maps`, {
+      params: {
+        fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+        filter: `id:in:[${chunk.map((vis) => vis.id).join(",")}]`,
+        paging: false,
+      },
+    });
+    mapConfigs.push(...response.data.maps);
+  }
+  return mapConfigs;
+}
+
+export function getIndicatorIdsFromVisualizations(visualizationConfigs: any[]) {
+  return _.compact(
+    _.flattenDeep(
+      visualizationConfigs.map((config) =>
+        config.dataDimensionItems.map(
+          (item: { indicator?: { id: string } }) => item.indicator?.id
+        )
+      )
+    )
+  );
+}
+
+export function getIndicatorIdsFromMaps(mapsConfig: any[]) {
+  return _.compact(
+    _.flattenDeep(
+      mapsConfig.map((config) =>
+        config.mapViews.map((view: any) =>
+          view.dataDimensionItems.map(
+            (item: { indicator?: { id: string } }) => item.indicator?.id
+          )
+        )
+      )
+    )
+  );
+}
+
+export async function getIndicatorConfigs(indicatorIds: string[]) {
+  if (indicatorIds.length < 30) {
+    const response = await dhis2Client.get<{ indicators: any[] }>(
+      `indicators`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${indicatorIds.join(",")}]`,
+          paging: false,
+        },
+      }
+    );
+    return response.data.indicators.map((indicator) => ({
+      ...indicator,
+      translations: [],
+    }));
+  }
+  const indicators = [];
+  const chunked = _.chunk(indicatorIds, 30);
+  for (const chunk of chunked) {
+    const response = await dhis2Client.get<{ indicators: any[] }>(
+      `indicators`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${chunk.join(",")}]`,
+          paging: false,
+        },
+      }
+    );
+    indicators.push(
+      ...response.data.indicators.map((indicator) => ({
+        ...indicator,
+        translations: [],
+      }))
+    );
+  }
+  return indicators;
+}
+
+export function getDataElementIdsFromMaps(mapsConfig: any[]) {
+  return _.compact(
+    _.flattenDeep(
+      mapsConfig.map((config) =>
+        config.mapViews.map((view: any) =>
+          view.dataDimensionItems.map(
+            (item: { dataElement?: { id: string } }) => item.dataElement?.id
+          )
+        )
+      )
+    )
+  );
+}
+
+export function getDataElementIdsFromVisualizations(
+  visualizationConfigs: any[]
+) {
+  return _.compact(
+    _.flattenDeep(
+      visualizationConfigs.map((config) =>
+        config.dataDimensionItems.map(
+          (item: { dataElement?: { id: string } }) => item.dataElement?.id
+        )
+      )
+    )
+  );
+}
+
+export async function getDataElementConfigs(dataElementIds: string[]) {
+  if (dataElementIds.length < 30) {
+    const response = await dhis2Client.get<{ dataElements: any[] }>(
+      `dataElements`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${dataElementIds.join(",")}]`,
+          paging: false,
+        },
+      }
+    );
+
+    return response.data.dataElements.map((dataElement) => ({
+      ...dataElement,
+      translations: [],
+    }));
+  }
+  const dataElements = [];
+  const chunked = _.chunk(dataElementIds, 30);
+  for (const chunk of chunked) {
+    const response = await dhis2Client.get<{ dataElements: any[] }>(
+      `dataElements`,
+      {
+        params: {
+          fields: ":owner,!createdBy,!lastUpdatedBy,!created,!lastUpdated",
+          filter: `id:in:[${chunk.join(",")}]`,
+          paging: false,
+        },
+      }
+    );
+    dataElements.push(
+      ...response.data.dataElements.map((dataElement) => ({
+        ...dataElement,
+        translations: [],
+      }))
+    );
+  }
+  return dataElements;
+}
